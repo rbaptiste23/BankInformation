bfe367ea2a223c66f12bd4c3b68daf2c7b49524d
rbaptiste23 <riley.bap@outlook.com>
2019-02-16 17:06:51 -0600

    Update App.java

--- src/main/java/com/codewrite/bb/App.java
+++ src/main/java/com/codewrite/bb/App.java
@@ -17,6 +17,7 @@ public class App {
 		System.out.println(bank.toString());
 		System.out.println(bank.toString());
 		System.out.println(bank.toString());
+		System.out.println(bank.toString());
 
 	}
 
